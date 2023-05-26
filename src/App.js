75b9534eca16e8f2796e6e54c0fc83dad16afc3f
padmalochan sahu <padmalochan.sahu@dianapps.com>
2023-05-26 10:08:33 +0530

    Number Game

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NumberGame from './Components/NumberGame';
+
+function App() {
+  return (
+    <>
+      <h1>Number Sequence Game</h1>
+      <br/>
+      <div id='game'>
+        <NumberGame />
+      </div>
+    </>
+  );
+}
+
+export default App;
